ca5e423cd3de3102ea98af96939e64f66990a8c5
xiaobin.zhao <xiaobin.zhao@miaoyun.io>
2025-05-27 14:38:30 +0800

    deepseek官方token计算；批量生成sglang bench_serving测试结果到excel；perfquery工具

--- batch_test_bench.py
+++ batch_test_bench.py
@@ -10,22 +10,24 @@ from argparse import ArgumentParser
 import pandas as pd
 # 需要预先安装sglang
 from sglang.bench_serving import run_benchmark, set_global_args
+
 # 定义参数
 input_output_pairs = [(1024, 256), (1024, 1024), (2048, 2048)]  # 输入/输出长度设置
 # 并发值设置,有一个1025的目的是为了实现并发和请求数都是1024的case
-concurrency_values = [1, 16, 32, 48, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 448, 512, 640, 768, 896, 1025, 1024]
+concurrency_values = [1, 16, 32, 48, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 448, 512, 640, 768, 896, 1025,
+                      1024]
 results = []
 
 parser = ArgumentParser(description="Benchmark the online serving throughput.")
 parser.add_argument(
-        "--model",
-        type=str,
-        help="Name or path of the model. If not set, the default model will request /v1/models for conf.",
-    )
+    "--model",
+    type=str,
+    help="Name or path of the model. If not set, the default model will request /v1/models for conf.",
+)
 
 parser.add_argument(
-        "--dataset-path", type=str, default="", help="Path to the dataset."
-    )
+    "--dataset-path", type=str, default="", help="Path to the dataset."
+)
 in_args = parser.parse_args()
 
 # 设置全局参数
@@ -48,7 +50,7 @@ args = argparse.Namespace(
     disable_ignore_eos=False,
     apply_chat_template=False,
     lora_name=None,
-    pd_seperated=False,
+    pd_separated=False,
     flush_cache=False,
     prompt_suffix="",
     warmup_requests=1,
@@ -70,8 +72,11 @@ set_global_args(args)
 
 # 使用ExcelWriter来逐次写入结果
 output_file = f"batch_{args.dataset_name}_{args.backend}_benchmark_results.xlsx"
+i = 1
 for input_len, output_len in input_output_pairs:
     for concurrency in concurrency_values:
+        args.seed = i
+        i += 1
         args.random_input_len = input_len
         args.random_output_len = output_len
         args.max_concurrency = concurrency
@@ -80,20 +85,22 @@ for input_len, output_len in input_output_pairs:
             args.num_prompts = 50
         elif concurrency in [1025]:
             args.num_prompts = concurrency - 1
-            args.max_concurrency = concurrency -1
+            args.max_concurrency = concurrency - 1
         else:
             args.num_prompts = concurrency * 2  # 请求个数是并发个数的2倍
 
-        print(f"\n\n === start benchmark for input_len={input_len}, output_len={output_len}, concurrency={concurrency}, num_prompts={args.num_prompts} === \n\n")
+        print(
+            f"\n\n === start benchmark for input_len={input_len}, output_len={output_len}, concurrency={concurrency}, num_prompts={args.num_prompts} === \n\n")
 
         result = run_benchmark(args)  # 调用sglang的run_benchmark函数
 
         # 提取需要的字段
         total_throughput = (result["total_input_tokens"] + sum(result["output_lens"])) / result["duration"]
         entry = {
+            "seed": args.seed,
             "Input len": input_len,
             "Output len": output_len,
-            "batch size": concurrency,
+            "batch size": args.max_concurrency,
             "requests": args.num_prompts,
             "TTFT(ms)": result["mean_ttft_ms"],
             "TPOT(ms)": result["mean_itl_ms"],
